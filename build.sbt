eb15f9605fb1572a87f8d9a52dc7ad988dec0a50
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-10-25 02:26:21 +0900

    Testing with parquet

--- build.sbt
+++ build.sbt
@@ -7,8 +7,18 @@ scalaVersion := "2.11.12"
 // resolvers += "The Graphene Extraction Tool" at "https://github.com/Lambda-3/Graphene"
 resolvers += "Local Maven" at Path.userHome.asFile.toURI.toURL + ".m2/repository"
 
+val SPARK_VERSION = "2.3.2"
+val JACKSON_VERSION = "2.8.8"
+
+dependencyOverrides ++= Seq(
+  "com.fasterxml.jackson.core" % "jackson-core" % JACKSON_VERSION,
+  "com.fasterxml.jackson.core" % "jackson-databind" % JACKSON_VERSION,
+  "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % JACKSON_VERSION
+)
+
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.3.2",
+  "org.apache.spark" %% "spark-core" % SPARK_VERSION,
+  "org.apache.spark" %% "spark-sql" % SPARK_VERSION,
   "org.lambda3.graphene" % "graphene-core" % "3.0.0-SNAPSHOT",
   "org.lambda3.graphene" % "graphene" % "3.0.0-SNAPSHOT"
 //  "org.lambda3.text.simplification" % "discourse-simplification" % "8.1.0" % "provided"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,81 @@
+import java.util
+
 import org.lambda3.graphene.core.Graphene
+import org.apache.spark.sql.{Row, SparkSession, types}
+
+
+import scala.collection.JavaConverters._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val graphene = new Graphene()
+    // Check arguments
+    if (args.length < 1) {
+      System.err.println("Usage: Main <file_path>")
+      System.exit(1)
+    }
 
-    val testString = "In which year were TV licences introduced in the UK?"
-    val result = graphene.doRelationExtraction(testString, false, false).serializeToJSON()
+    val spark = SparkSession
+      .builder()
+      .appName("Exobrain Graphene Processer")
+      .master("local[*]")
+      .getOrCreate()
+    val sc = spark.sparkContext
 
-    println(result)
+    val file_path = args(0)
+
+    val schema = types.StructType(
+      types.StructField("file", types.DataTypes.StringType, true) ::
+        types.StructField("content", types.DataTypes.StringType, true) :: Nil
+    )
+    val rdd = spark.read.schema(schema).parquet("data/wikipedia-train/**")
+    rdd.show()
+
+    import spark.implicits._
+    val results = rdd.mapPartitions(rows => {
+      new util.ArrayList[String]().iterator.asScala
+    }).collect()
   }
+
+  //  def test(): Unit = {
+  //    val graphene = new Graphene()
+  //
+  //    val testString = "In which year were TV licences introduced in the UK?"
+  //    val result = graphene.doRelationExtraction(testString, false, false).serializeToJSON()
+  //
+  //    println(result)
+  //  }
+}
+
+object GrapheneProcessor {
+  def process(rows: Iterator[Row]): Iterator[String] = {
+    val results = new util.ArrayList[String]
+    if (rows.isEmpty) {
+      return results.iterator.asScala
+    }
+
+    rows.map(row => {
+      val file = row(0).toString
+      val content = row(1).toString
+
+      results.add(file + "\t" + content)
+    })
+
+    return results.iterator.asScala
+  }
+
+  //  def process(rows: Iterator[Row]): Iterator[String] = {
+  //    val graphene = new Graphene()
+  //    val results = new util.ArrayList[String]
+  //
+  //    rows.map(row => {
+  //      val file = row(0).toString
+  //      val content = row(1).toString
+  //      val result = graphene.doRelationExtraction(content, true, false).serializeToJSON()
+  //
+  //      results.add(file + "\t" + result)
+  //    })
+  //
+  //
+  //    return results.iterator.asScala
+  //  }
 }
