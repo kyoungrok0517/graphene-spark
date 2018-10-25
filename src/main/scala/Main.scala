653400e55f3c511cf4e2c4d8f7c185fa8ffb7f5a
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-10-25 12:42:20 +0900

    Testing with parquet

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,5 @@
-import java.util
-
 import org.lambda3.graphene.core.Graphene
-import org.apache.spark.sql.{Row, SparkSession, types}
-
-import scala.collection.JavaConverters._
-import scala.collection.mutable.ListBuffer
+import org.apache.spark.sql.{DataFrame, Row, SparkSession, types}
 
 object Main {
   val schema = types.StructType(
@@ -14,34 +9,58 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     // Check arguments
-    if (args.length < 1) {
-      System.err.println("Usage: Main <file_path>")
+    if (args.length < 4) {
+      System.err.println("Usage: Main <data_path> <n_threads>")
       System.exit(1)
     }
+
+    // get args
+    val data_path = args(0)
+//    val target_partition = args(1).toInt
+//    val total_partition = args(2).toInt
+    val n_threads = args(1).toInt
 
     val spark = SparkSession
       .builder()
       .appName("Exobrain Graphene Processer")
-      .master("local[*]")
+      .master(s"local[$n_threads]")
       .getOrCreate()
     val sc = spark.sparkContext
 
-    val file_path = args(0)
+    val df = spark.read.schema(schema).parquet(data_path).toDF()
 
-    val df = spark.read.schema(schema).parquet(file_path)
+    // 여러 컴퓨터에 분산하기 위한 코드
+//    var df_partitions = Seq[DataFrame]()
+//    val frac = 1.0 / total_partition
+//    val seed = 42  // subset 결과가 항상 동일하도록
+//    for (_ <- 1 to total_partition) {
+//      val df_ = df.sample(false,0.25, seed = seed).toDF()
+//      df_partitions = df_partitions :+ df_
+//    }
+//    // 처리할 파티션 선택
+//    val target_df = df_partitions(target_partition)
+
+    // 처리 시작
+    val totalCount = sc.broadcast(df.count())
+    val finishedFilesCounter = sc.longAccumulator("finishedFilesCounter")
 
     import spark.implicits._
-    df.rdd.mapPartitions(rows => {
+    df.mapPartitions(rows => {
       val graphene = new Graphene()
 
       val results = rows.map(row => {
         val file = row(0).toString
-        val content = row(1).toString
-        val lines = content.split("\n")
+        val lines = row(1).toString.split("\n")
         val results_ = lines.map(line => {
           val res_json = graphene.doRelationExtraction(line, true, false).serializeToJSON()
+
           (file, res_json)
         })
+
+        // update the counter
+        finishedFilesCounter.add(1)
+
+        // return
         results_
       })
       results.flatten
