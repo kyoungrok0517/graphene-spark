02fdb69115f1bdf2d22fb20cff25650a7daa5582
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-10-29 20:37:23 +0900

    trivial

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,35 +10,22 @@ object Main {
   def main(args: Array[String]): Unit = {
     // Check arguments
     if (args.length < 2) {
-      System.err.println("Usage: Main <data_path> <n_threads>")
+      System.err.println("Usage: Main <data_path> <out_path>")
       System.exit(1)
     }
 
     // get args
     val data_path = args(0)
-//    val target_partition = args(1).toInt
-//    val total_partition = args(2).toInt
-    val n_threads = args(1).toInt
+    val out_path = args(1)
 
     val spark = SparkSession
       .builder()
       .appName("Exobrain Graphene Processer")
-      .master(s"local[$n_threads]")
+      .master(s"local[*]")
       .getOrCreate()
     val sc = spark.sparkContext
 
     val df = spark.read.schema(schema).parquet(data_path).toDF()
-
-    // 여러 컴퓨터에 분산하기 위한 코드
-//    var df_partitions = Seq[DataFrame]()
-//    val frac = 1.0 / total_partition
-//    val seed = 42  // subset 결과가 항상 동일하도록
-//    for (_ <- 1 to total_partition) {
-//      val df_ = df.sample(false,0.25, seed = seed).toDF()
-//      df_partitions = df_partitions :+ df_
-//    }
-//    // 처리할 파티션 선택
-//    val target_df = df_partitions(target_partition)
 
     // 처리 시작
     val totalCount = sc.broadcast(df.count())
@@ -64,6 +51,6 @@ object Main {
         results_
       })
       results.flatten
-    }).toDF("file", "graphene").write.option("compression", "snappy").parquet("./output")
+    }).toDF("file", "graphene").write.option("compression", "snappy").parquet(out_path)
   }
 }
