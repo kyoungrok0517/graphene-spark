2457af73c499e797d8dd22ea204101223f3d1fb6
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2018-11-08 16:38:30 +0900

    remove run scripts

--- run2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-N_THREADS=${1}
-sbt "runMain Main ./data/wikipedia-train/2 $N_THREADS"
--- run3.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-N_THREADS=${1}
-sbt "runMain Main ./data/wikipedia-train/3 $N_THREADS"
